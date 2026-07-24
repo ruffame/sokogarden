c28372af65b43ed7a6e175b15b476570e101b794
ruffame <meruffa36@gmail.com>
2026-07-24 12:41:43 +0300

    game

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -8,39 +8,39 @@ const Videos = () => {
       <section className='row'>
         {/* child 1  */}
         <div className="col-md-4 mb-4">
-          <iframe width="450" height="315" src="https://www.youtube.com/embed/2SDJqisfezY?si=3oY-qgnxu8HzmvrX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/2SDJqisfezY?si=3oY-qgnxu8HzmvrX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <h5 className='mt-2 text-center'>farm Robots</h5>
           <p className="text-center ">Learn how robots are transforming modern farming</p>
         </div>
         {/* child 2  */}
         <div className="col-md-4 mb-4">
-          <iframe width="450" height="315" src="https://www.youtube.com/embed/pgNCgJG0vnY?si=8Wg7nZSsAah1CuyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/pgNCgJG0vnY?si=8Wg7nZSsAah1CuyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <h5 className='mt-2 text-center'>farm Robots</h5>
           <p className="text-center ">Learn how robots are transforming modern farming</p>
         </div>
         {/* child 3 */}
         <div className="col-md-4 ">
-          <iframe width="450" height="315" src="https://www.youtube.com/embed/WS9aGypJPJ4?si=y6uVmLeGZbxOsRA7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/WS9aGypJPJ4?si=y6uVmLeGZbxOsRA7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <h5 className='mt-2 text-center'>farm Robots</h5>
           <p className="text-center ">Learn how robots are transforming modern farming</p>
         </div>
-      </section> 
+      </section>
       <section className='row'>
         {/* child 1  */}
         <div className="col-md-4 mb-4">
-          <iframe width="450" height="315" src="https://www.youtube.com/embed/ztjfwecrY8E?si=WAlBb2YUdjeNR0PE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/ztjfwecrY8E?si=WAlBb2YUdjeNR0PE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <h5 className='mt-2 text-center'>farm Robots</h5>
           <p className="text-center ">Learn how robots are transforming modern farming</p>
         </div>
         {/* child 2  */}
         <div className="col-md-4 mb-4">
-          <iframe width="450" height="315" src="https://www.youtube.com/embed/hvoD7ehZPcM?si=YSAZ0utsGRLrY5Kf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/hvoD7ehZPcM?si=YSAZ0utsGRLrY5Kf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <h5 className='mt-2 text-center'>farm Robots</h5>
           <p className="text-center ">Learn how robots are transforming modern farming</p>
         </div>
         {/* child 3 */}
         <div className="col-md-4 ">
-          <iframe width="450" height="315" src="https://www.youtube.com/embed/1R4MdobS1nI?si=EIKrKup5gqnadUex&amp;start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/1R4MdobS1nI?si=EIKrKup5gqnadUex&amp;start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <h5 className='mt-2 text-center'>farm Robots</h5>
           <p className="text-center ">Learn how robots are transforming modern farming</p>
         </div>
