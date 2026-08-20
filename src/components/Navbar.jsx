b84ce862fc99e0ac67805c6450d9a2793b321f6b
ruffame <meruffa36@gmail.com>
2026-08-20 09:11:10 +0300

    game

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
                             
                             <a href="/addproduct" class="nav-link"><h4>addproduct</h4></a>
                             <a href='/videos' class="nav-link"><h4>Videos</h4></a> <br />
-                            <a href="https://www.facebook.com" style={{marginLeft:"670px"}}><img src="images/fb.png" alt="" width="40px"/></a>
+                            <a href="https://www.facebook.com" style={{marginLeft:"470px"}}><img src="images/fb.png" alt="" width="40px"/></a>
                             <a href="https://www.instagram.com" style={{marginLeft:""}}><img src="images/ins.png" alt="" width="40px"/></a>
                            {user ? (
                               <>
